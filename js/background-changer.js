4ed0855191e065f0a6de26e590c5e30bb656b500
Pivozavr2003 <alexsuvorov2506@gmail.com>
2023-10-18 13:36:13 +0300

    edit: index.html background-changer.js
    index.html:
    	a few fixes
    background-changer.js:
    	fixed logic error so theme can change correctly

--- js/background-changer.js
+++ js/background-changer.js
@@ -5,9 +5,10 @@ var email_form = document.querySelector(".email-form"),
     console.log(email_form__input_email.classList);
 
     document.addEventListener("dblclick", (e) => {
-        if(e.target.classList.contains("email-form"))
+        if(e.target.classList.contains("email-form")) {
             email_form.classList.toggle("email-form_theme_dark");
             email_form__input_email.classList.toggle("email-form__input-email_theme_dark");
             email_form__check_button.classList.toggle("email-form__check-button_theme_dark");
             console.log("theme_dark");
+        }
     });
